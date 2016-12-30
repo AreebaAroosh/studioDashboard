e6d4469bff9dbd4bb1cc9a09b85f19a1ce9040b9
root <born2net@gmail.com>
2016-12-30 13:49:09 -0800

    clean ups

--- src/store/application-state.ts
+++ src/store/application-state.ts
@@ -1,13 +1,10 @@
-import {UiState, INITIAL_UI_STATE} from "./ui-state";
 import {StoreData, INITIAL_STORE_DATA, INITIAL_APP_DB, IAppDb} from "./store-data";
 
 export interface ApplicationState {
-    uiState: UiState,
     storeData: StoreData
     appDb: IAppDb
 }
 export const INITIAL_APPLICATION_STATE: ApplicationState = {
-    uiState: INITIAL_UI_STATE,
     storeData: INITIAL_STORE_DATA,
     appDb: INITIAL_APP_DB
 };
